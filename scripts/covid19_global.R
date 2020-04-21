b6699a09e4d7bd6b81382939ce99dd45dac50f56
Jesús Nieto <jesusmanuel.nieto@gmail.com>
2020-04-21 22:05:29 +0200

    Actualización de datos 21abr20

--- scripts/covid19_global.R
+++ scripts/covid19_global.R
@@ -13,6 +13,7 @@
   @sources:
   {
     @data:         https://www.kaggle.com/sudalairajkumar/novel-corona-virus-2019-dataset#covid_19_data.csv,
+    @dataZip:      https://www.kaggle.com/sudalairajkumar/novel-corona-virus-2019-dataset/download,
     @deathTax:     http://www.telemadrid.es/coronavirus-covid-19/mortalidad-COVID-19-Wuhan-menor-estimado-0-2214678548--20200319021221.html
   }
 "
